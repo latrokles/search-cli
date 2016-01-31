149ed4842c6cd1e2b60d909e0ec76d5b2867b5eb
latrokles <latrokles@gmail.com>
2016-01-30 22:45:29 -0800

    use search lib in main, handle opts and missing args

--- index.js
+++ index.js
@@ -3,13 +3,33 @@
 'use strict';
 
 var program = require('commander');
-var pjson = require('./package.json')
+var pjson = require('./package.json');
+var search = require('./lib/search');
 
 program
   .version(pjson.version)
   .arguments('<query>')
-  .option('-a, --application <application>', 'application to open results with')
   .option('-s, --service <service>', 'to perform search on (e.g. google, npm)')
   .option('-c, --configure', 'configure a new service')
   .option('-l, --list', 'list configured services')
   .parse(process.argv);
+
+/** 
+ * list or configure services
+ */
+if (program.list) search.list();
+if (program.configure) search.configure();
+
+/**
+ * handle missing args
+ */ 
+if (!program.args.length) {
+  console.error('missing query...');
+  console.log(program.help());
+  process.exit(1);
+}
+
+/**
+ * run query
+ */
+search.query(program.args, program.service);
