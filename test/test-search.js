11e5d0db71251faa62e572fb7dd58f3820679684
latrokles <latrokles@gmail.com>
2016-01-31 13:56:06 -0800

    and it breaks, lol

--- test/test-search.js
+++ test/test-search.js
@@ -4,10 +4,11 @@ var search = require('../lib/search');
 
 describe('Search', function () {
   describe('.availableServices()', function () {
-    it('returns an array of configured services', function () {
+    it('returns non-empty array of configured services', function () {
       var services = search.availableServices();
 
       expect(services).to.be.a('array');
+      expect(services).to.have.length.above(0);
     });
   });
 });
