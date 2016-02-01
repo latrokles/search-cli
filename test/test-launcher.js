642c73aad21130196bbe30c2c470275b2e67a86f
latrokles <latrokles@gmail.com>
2016-01-31 21:24:35 -0800

    failing test

---
+++ test/test-launcher.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect;
+var launcher = require('../lib/launcher');
+
+describe('launcher', function () {
+  describe('.command()', function () {
+    it('returns the launch command to be used', function () {
+      var actual = launcher.command();
+
+      expect(actual).to.be.a('string');
+    });
+  });
+});
