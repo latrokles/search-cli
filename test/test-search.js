4221663618700a83c7efde5668acd69f2caaa290
latrokles <latrokles@gmail.com>
2016-01-31 17:00:05 -0800

    moved the config file test setup/teardown to npm scripts
    - I was getting inconsitent test results with the readstrem.pipe.writestream
      in my before function. maybe because of its async behavior.
    
    - using pretest/posttest guarantees that set up / tear down is handled
      before the test even runs, and since it's a unix command, it just works.
    
    - one downside, it's not exactly portable. I can replace it with a more portable
      solution later.

--- test/test-search.js
+++ test/test-search.js
@@ -1,25 +1,13 @@
 
 var expect = require('chai').expect;
 var search = require('../lib/search');
-var fs = require('fs');
 
-const CONFIG_FILE = './config.json';
 const TEST_CONFIG_FILE = './test/test-config.json';
 
 describe('Search', function () {
   before(function () {
-    // copy config file to staging area
-    fs
-      .createReadStream(CONFIG_FILE)
-      .pipe(fs.createWriteStream(TEST_CONFIG_FILE));
-
     // overwrite configuration file for test purposes.
     search.configuration = TEST_CONFIG_FILE;
-  });
-
-  after(function () {
-    // remove the config file from staging area
-    fs.unlinkSync(TEST_CONFIG_FILE);
   });
 
   describe('.availableServices()', function () {
@@ -28,6 +16,8 @@ describe('Search', function () {
 
       expect(services).to.be.a('array');
       expect(services).to.have.length.above(0);
+      expect(services).to.include('amazon');
+      expect(services).to.include('google');
     });
   });
 });
