6a7ea716c5ebe068785d4146ffc13d829156cbb9
latrokles <latrokles@gmail.com>
2016-02-03 22:13:23 -0800

    first pass at some refactoring
    - moved main app logic out of index into its own module.
    - doesn't change things much, but makes some planned changes to index.js easier
      (if only by virtue of there not being much code there now).

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
 'use strict';
 
 var program = require('commander');
-var prompt = require('prompt');
-var util = require('util');
 
-var launch = require('./lib/launcher').launch;
 var pjson = require('./package.json');
-var search = require('./lib/search');
+var cli = require('./lib/cli.js');
 
 program
   .version(pjson.version)
@@ -22,57 +19,9 @@ program
  * list or configure services
  */
 if (program.list) {
-  list();
+  cli.list();
 } else if (program.configure) {
-  configure();
+  cli.configure();
 } else {
-  runSearch();
-}
-
-function list() {
-  console.log('these are the services you can search:');
-  console.log('');
-  search
-    .getAvailableServices()
-    .forEach(function (serviceName) {
-      console.log(serviceName);
-    });
-  process.exit(0);
-}
-
-function configure() {
-  console.log('enter new service information:');
-  prompt.start();
-  prompt.get(['service', 'url'], function (err, result) {
-    if (err) {
-      onError(err);
-    }
-
-    console.log('configuring new service...');
-    search.addNewService(result.service, result.url);
-    process.exit(0);
-  });
-}
-
-function runSearch() {
-  if (!program.args.length) {
-    onError('missing query...');
-  }
-  var query = program.args.join(' ');
-  var service = program.service;
-  var searchURI = search.getServiceURI(query, service);
-
-  if (searchURI) {
-    launch(searchURI);
-    process.exit(0);
-  }
-  var msg = '%s is not configured, please use --configure option to add it as a new service';
-  onError(util.format(msg, service));
-}
-
-function onError(errorMessage) {
-  console.log('');
-  console.error(errorMessage);
-  console.log(program.help());
-  process.exit(1);
+  cli.search(program.service, program.args);
 }
